ab309bc2ce19bde67aad46ae3e6ee1944d2fcb01
Michael <michaeljohneduave@gmail.com>
2025-02-11 06:09:39 -0800

    Remove binary search implementation

--- src/algos/binarysearch.ts
+++ src/algos/binarysearch.ts
@@ -1,22 +1,3 @@
 export default function binarysearch(arr: number[], needle: number): boolean {
-  let lo = 0;
-  let hi = arr.length - 1;
 
-  while (lo <= hi) {
-    const m = lo + Math.floor((hi - lo) / 2);
-
-    if (arr[m] === needle) {
-      return true;
-    }
-
-    // Right side
-    if (arr[m] < needle) {
-      lo = m + 1;
-    // Left side
-    } else {
-      hi = m - 1;
-    }
-  }
-
-  return false;
 }
