46011a904dbaa29e47e89093a77ff89e5edb6643
Michael <michaeljohneduave@gmail.com>
2025-06-27 10:09:01 -0700

    Remove debugger

--- src/__tests__/bubble.test.ts
+++ src/__tests__/bubble.test.ts
@@ -1,9 +1,8 @@
 import bubble_sort from "../sorting/bubble";
 
-test("bubble-sort", function () {
-  const arr = [9, 3, 7, 4, 69, 420, 42];
+test("bubble-sort", () => {
+	const arr = [9, 3, 7, 4, 69, 420, 42];
 
-  debugger;
-  bubble_sort(arr);
-  expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+	bubble_sort(arr);
+	expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
 });
