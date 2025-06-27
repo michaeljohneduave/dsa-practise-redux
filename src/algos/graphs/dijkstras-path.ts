34032fb157faddcf6ff02c63b836682f26e2b05d
Michael <michaeljohneduave@gmail.com>
2025-06-27 10:06:34 -0700

    Formatting

--- src/algos/graphs/dijkstras-path.ts
+++ src/algos/graphs/dijkstras-path.ts
@@ -1,13 +1,13 @@
 
 type ShortestPath = {
-  path: number[];
-  distance: number
-}
+	path: number[];
+	distance: number;
+};
 
 type Node = {
-  val: number;
-  distance: number;
-}
+	val: number;
+	node: number;
+};
 
 export default function dijkstras (graph: WeightedAdjacencyList, source: number, sink: number): ShortestPath | null {
 }
